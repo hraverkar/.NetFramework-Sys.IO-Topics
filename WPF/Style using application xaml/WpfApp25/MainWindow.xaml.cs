0b3c48eb95724acc9aa871c2a98d462707dda6a5
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-01-15 22:14:05 +0530

    Style Assign applied in App.xaml
    Style Assign in App.xaml.

---
+++ WPF/Style using application xaml/WpfApp25/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp25
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      var btn = (Button) e.Source;
+      MessageBox.Show(btn.Content.ToString());
+    }
+  }
+}
