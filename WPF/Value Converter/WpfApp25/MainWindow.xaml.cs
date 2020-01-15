085747e758bc45840a7b85fe29a946254f99e103
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-01-15 23:25:16 +0530

    Value Converter example
    Value Converter example with checkbox value

---
+++ WPF/Value Converter/WpfApp25/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApp25
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+
+  public class YesNoToBooleanConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      switch (value.ToString().ToLower())
+      {
+        case "yes":
+        case "oui":
+          return true;
+
+        case "no":
+        case "non":
+          return false;
+      }
+
+      return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (value is bool b )
+      {
+        if ( b == true)
+        {
+          return "yes";
+        }
+        else
+        {
+          return "no";
+        }
+      }
+      return "no";
+      }
+  }
+}
