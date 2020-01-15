0d751489a1eef252a4a07b88a0fdc4df726d7ca5
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-01-15 22:15:02 +0530

    Window resource property Assign in XAML
    Window resource Property assing in Xaml only

---
+++ WPF/ExplicitlyusingStyle/ExplicitlyusingStyle/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ExplicitlyusingStyle
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      var btn = (Button) e.Source;
+      var value = btn.Content.ToString();
+      MessageBox.Show(value);
+    }
+  }
+}
