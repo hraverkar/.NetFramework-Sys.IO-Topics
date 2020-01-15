f584d2c51ea76679778f23d55f128982de7458ea
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-01-15 22:10:27 +0530

    Grid Event on MouseClickDown
    Event emmiter click on grid.

---
+++ WPF/EventEmitter/EventEmitter/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventEmitter
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void PnlmainGrid_OnMouseUp(object sender, MouseButtonEventArgs e)
+    {
+      MessageBox.Show("You clicked at me " + e.GetPosition(this).ToString());
+    }
+  }
+}
