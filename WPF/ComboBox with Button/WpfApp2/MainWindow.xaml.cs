04c70ec8baa81c5d26177d262080e4b6af9a967d
Harshal <harshal.raverkar@gmail.com>
2020-01-17 13:18:00 +0530

    Combo box with button
    Combo box with button

---
+++ WPF/ComboBox with Button/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      cbbox.ItemsSource = typeof(Colors).GetProperties();
+    }
+
+    private void cbbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      Color selectedColor = (Color)(cbbox.SelectedItem as PropertyInfo).GetValue(null, null);
+      this.Background = new SolidColorBrush(selectedColor);
+    }
+
+    private void BtnPrevious_Click(object sender, RoutedEventArgs e)
+    {
+      if (cbbox.SelectedIndex > 0)
+        cbbox.SelectedIndex = cbbox.SelectedIndex - 1;
+    }
+
+    private void btnNext_Click(object sender, RoutedEventArgs e)
+    {
+      if (cbbox.SelectedIndex < cbbox.Items.Count - 1)
+        cbbox.SelectedIndex = cbbox.SelectedIndex + 1;
+    }
+
+    private void btnBlue_Click(object sender, RoutedEventArgs e)
+    {
+      cbbox.SelectedItem = typeof(Colors).GetProperty("Blue");
+    }
+  }
+}
---
+++ WPF/CustomCheckbox Content/CustomCheckbox Content/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomCheckbox_Content
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      combo.ItemsSource = typeof(Colors).GetProperties();
+    }
+
+    private void combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      var cmd = sender as ComboBox;
+      var str = cmd.SelectedItem.ToString();
+      MessageBox.Show(str);
+    }
+  }
+}
